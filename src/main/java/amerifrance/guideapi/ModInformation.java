eab78d7dc818cb789400d5bbd2c8b643122f5ea1
bombcar <github@bombcar.com>
2022-02-05 09:40:41 -0600

    builds

--- src/main/java/amerifrance/guideapi/ModInformation.java
+++ src/main/java/amerifrance/guideapi/ModInformation.java
@@ -2,13 +2,13 @@ package amerifrance.guideapi;
 
 public class ModInformation {
 
-    public static final String NAME = "Guide-API";
-    public static final String ID = "guideapi";
+    public static final String NAME = "GRADLETOKEN_MODNAME";
+    public static final String ID = "GRADLETOKEN_MODID";
     public static final String CHANNEL = "GuideAPI";
     public static final String COMMONPROXY = "amerifrance.guideapi.proxies.CommonProxy";
     public static final String CLIENTPROXY = "amerifrance.guideapi.proxies.ClientProxy";
     public static final String DEPEND = "";
-    public static final String VERSION = "@VERSION@";
+    public static final String VERSION = "GRADLETOKEN_VERSION";
     public static final String TEXLOC = "guideapi:";
     public static final String GUITEXLOC = TEXLOC + "textures/gui/";
 }
